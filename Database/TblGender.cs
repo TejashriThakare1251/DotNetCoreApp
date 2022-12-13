1252d3a39b2249ffa409ff72bdca333f6f4d5d40
tejas <tejas@LAPTOP-A0O5CFH4>
2022-12-13 19:12:29 +0530

    Add project files.

---
+++ Database/TblGender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreApp.Database
+{
+    public partial class TblGender
+    {
+        public TblGender()
+        {
+            TblUserRegsitrations = new HashSet<TblUserRegsitration>();
+        }
+
+        public int GenderId { get; set; }
+        public string Gender { get; set; } = null!;
+
+        public virtual ICollection<TblUserRegsitration> TblUserRegsitrations { get; set; }
+    }
+}
